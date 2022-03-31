7af38344a2abb7ac47a3558604cdb4e40d6236e4
sajid-techis <sajid@techis.io>
2022-04-01 01:22:16 +0530

    added Project

---
+++ shareme_frontend/src/components/Feed.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { client } from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
+
+const Feed = () => {
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
+  const [pins, setPins] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    const query = searchQuery(categoryId);
+
+    client.fetch(query).then((data) => {
+      setPins(data);
+      setLoading(false);
+    });
+    if (categoryId) {
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
+  if (!pins?.length) return <h2>Opps No Pins In This Category</h2>
+  if (loading)
+    return <Spinner message="We are adding new ideas to your feed" />;
+    return (
+        <div>
+            {pins && <MasonryLayout pins={pins} />}
+        </div>
+  )
+};
+
+export default Feed;
